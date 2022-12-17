d12fcd45d54705b12b8a4fde73a12ed1f07cc6fb
Divya Swarnkar <kdivyaswarnkar@gmail.com>
2022-12-17 09:55:02 +0530

    authentication

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,30 +1,36 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { catchError } from "rxjs";
+import { Router } from "@angular/router";
+import { BehaviorSubject, catchError, Subject, tap } from "rxjs";
 import { throwError } from 'rxjs';
+import { User } from "./user.model";
 
 
 export interface AuthResponseData{
+   kind:string;
    idToken:string;
    email:string;
    refreshToken:string;
    expiresIn:string;
    localId:string;
    registered?:boolean;
-
 }
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-    constructor(private http: HttpClient) { }
+  user =new BehaviorSubject<User>(null);
+  private tokenExpirationTimer:any;
+  
+    constructor(private http: HttpClient,private router:Router) { }
     signup(email:string,password:string) {
        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB8QdWCXo3s-sv5udSG_q64QGK8nl18cGg',
         {
             email:email,
             password:password,
             returnSecureToken:true
-
-        }).pipe(catchError(this.handleError));
+        }).pipe(catchError(this.handleError),tap(resData=>{
+         this.handleAuthentication(resData.email,resData.localId,resData.idToken,+resData.expiresIn);
+        }));
     }
 
     login(email:string,password:string)
@@ -34,8 +40,68 @@ export class AuthService {
          email:email,
          password:password,
          returnSecureToken:true
-     }).pipe(catchError(this.handleError));
+     }).pipe(catchError(this.handleError),tap(resData=>{
+      this.handleAuthentication(resData.email,resData.localId,resData.idToken,+resData.expiresIn);
+     }));
     }
+
+  autoLogin()
+  {
+  const userData:{
+    email:string;
+    id:string;
+    _token:string;
+    _tokenExpirationDate:string;
+  }= JSON.parse(localStorage.getItem('userData'));
+  if(!userData)
+  {
+   return;
+  }
+  const loadedUser=new User(userData.email,userData.id,userData._token,new Date(userData._tokenExpirationDate));
+ 
+  if(loadedUser.token)
+  {
+   this.user.next(loadedUser);
+   const expirationDuration =
+        new Date(userData._tokenExpirationDate).getTime() -
+        new Date().getTime();
+      this.autoLogOut(expirationDuration);
+  }
+}
+
+
+
+  logout()
+  {
+    this.user.next(null);
+    this.router.navigate(['/auth']);
+    localStorage.removeItem('userData');
+    if(this.tokenExpirationTimer)
+    {
+    clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer=null;
+  }
+
+   autoLogOut(expirationDuration:number)
+   {
+    console.log(expirationDuration);
+   this.tokenExpirationTimer= setTimeout(()=>{
+      this.logout();
+    },expirationDuration);
+   }
+
+
+  private handleAuthentication(email:string,userId:string,token:string,expiresIn:number)
+  {
+    const expirationDate=new Date(new Date().getTime()+ expiresIn*1000);
+    const user=new User(email,userId,token,expirationDate);
+    this.user.next(user);
+    this.autoLogOut(expiresIn*1000);
+    localStorage.setItem('userData',JSON.stringify(user));
+  }
+
+
 
     private handleError(errorRes:HttpErrorResponse)
     {
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,24 +1,55 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+import { User } from '../auth/user.model';
 import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent
+export class HeaderComponent implements OnInit, OnDestroy
 {
+  isAuthenticated=false;
+ user: User;
+  private usersub:Subscription;
  //@Output() featureSelected=new EventEmitter<string>();
   // onSelect(feature:string)
   // {
   //   this.featureSelected.emit(feature);
   // }
 
-  constructor(private dataStorageService:DataStorageService){}
+  constructor(private dataStorageService:DataStorageService,private authService:AuthService){}
+  ngOnDestroy(): void {
+   this.usersub.unsubscribe();
+  }
+
+ ngOnInit(): void {
+//  this.usersub=  this.authService.user.subscribe(user=>{
+//   this.isAuthenticated=!!user;
+//   console.log(!user);
+//   console.log(!!user);
+//  });
+ //this.usersub = this.authService.user.subscribe(user => this.user = user);
+ console.log(this.isAuthenticated);
+ this.usersub = this.authService.user.subscribe(user => {
+   console.log(user);
+   this.isAuthenticated = !user ? false : true; // !!user;
+   console.log(!user);
+   console.log(!!user);
+ });
+ }
 
   onSaveData()
   {
    this.dataStorageService.storeRecipes();
   }
+
+  onLogout()
+  {
+    this.authService.logout();
+  }
+
   onFetchData()
   {
     this.dataStorageService.fetchRecipes().subscribe();
